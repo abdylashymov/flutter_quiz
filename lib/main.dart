47a26471a4ab02095418a6fbcbda8003a62336d7
Elchibek <abdylashymov@gmail.com>
2024-08-09 23:24:24 +0600

    quiz_app_added

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_application/app.dart';
+//import 'package:flutter_application_quiz/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
